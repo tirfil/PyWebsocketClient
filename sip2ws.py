a67716156e4188ec76c528bd5664bb9e50dbbc6f
tirfil <philippe.thirion@laposte.net>
2015-02-06 11:58:05 +0100

    manage headers (this module acts like a proxy)

--- sip2ws.py
+++ sip2ws.py
@@ -32,7 +32,7 @@ or proxy   | port|         |           |<========> Softphone SIP/UDP
 # -----------------------------------------------------
 # Softphone: UDP - proxy = SERVER_HOST:SERVER_PORT
 # SIP server: websocket port = CLIENT_HOST:CLIENT_PORT
-SERVER_HOST, SERVER_PORT = '0.0.0.0', 5060
+SERVER_HOST, SERVER_PORT = '0.0.0.0', 8060
 CLIENT_HOST, CLIENT_PORT = '172.26.158.24', 5060
 
 import wsclient
@@ -44,10 +44,13 @@ import string
 
 rx_via = re.compile("^Via:")
 rx_contact = re.compile("^Contact:")
+rx_record_route = re.compile("^Record-Route:")
 rx_viaproto = re.compile("SIP/2.0/([^ ]*)")
-rx_transportproto = re.compile("transport=([^;$>]*)")
+rx_transportproto = re.compile("transport=([^;$> ]*)")
 
 context = {}
+recordroute = ""
+#topvia = ""
 
 class WSListener(threading.Thread):
     def __init__(self,csock,parent):
@@ -84,8 +87,16 @@ class UDPHandler(SocketServer.BaseRequestHandler):
         else:
             print "handshake request issue"
             
+        
+    def _data2list(self,data):
+        return data.split("\r\n")
+        
+    def _list2data(self,list):
+        return "\r\n".join(list)
+            
     def _filter(self,data,proto):
-        list = data.split("\r\n")
+        #list = data.split("\r\n")
+        list = self._data2list(data)
         size = len(list)
         for index in range(size):
             line = list[index]
@@ -94,14 +105,38 @@ class UDPHandler(SocketServer.BaseRequestHandler):
                 if md:
                     p = md.group(1)
                     list[index] = line.replace(p,string.upper(proto))
-            if rx_contact.search(line) or (index == 0):
+            if (index == 0) or rx_contact.search(line) or rx_record_route.search(line):
                 md = rx_transportproto.search(line)
                 if md:
                     p = md.group(0)
                     proto1 = "transport=%s" % proto
                     list[index] = line.replace(p,string.lower(proto1))
         #list.append("")
-        return "\r\n".join(list)
+        #return "\r\n".join(list)
+        return self._list2data(list)
+    
+    def _clean_headers(self,data):
+        # record-route and top via
+        vias = []
+        listin = self._data2list(data)
+        listout = []
+        for line in listin:
+            # erase all record-route
+            if rx_record_route.search(line):
+                pass
+            elif rx_via.search(line):
+                vias.append(line)
+            else:
+                listout.append(line)
+        # erase top via except if only one
+        if len(vias) > 1:
+            vias.pop(0)
+        vias.reverse()
+        for str in vias:
+            listout.insert(1,str)
+        # insert record-route
+        listout.insert(1,recordroute)
+        return self._list2data(listout)
         
     def receive(self,data):
         if self.ws:
@@ -114,6 +149,7 @@ class UDPHandler(SocketServer.BaseRequestHandler):
             elif status == 8:
                 # receive decoded frame 
                 wsdata = self.ws.result()
+                wsdata = self._clean_headers(wsdata)
                 wsdata = self._filter(wsdata,"UDP")
                 print "received: \n%s" % wsdata
                 #print self.client_address
@@ -143,6 +179,13 @@ class UDPHandler(SocketServer.BaseRequestHandler):
             self._connect()
         else:
             (self.csock,self.ws) = context[self.client_address]
+                        
+            """
+            list = self._data2list(data)
+            list.insert(1,recordroute)
+            #list.insert(1,)
+            data = self._list2data(list)
+            """     
             data = self._filter(data,"WS")
             self.ws.sendData(data)
             if self.ws.status() == 3:
@@ -151,5 +194,11 @@ class UDPHandler(SocketServer.BaseRequestHandler):
                 self.csock.send(wsdata)
 
 if __name__ == "__main__":
+    hostname = socket.gethostname()
+    print hostname
+    ipaddress = socket.gethostbyname(hostname)
+    print ipaddress
+    recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress,SERVER_PORT)
+    #topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress,SERVER_PORT)
     server = SocketServer.UDPServer((SERVER_HOST, SERVER_PORT), UDPHandler)
     server.serve_forever()
